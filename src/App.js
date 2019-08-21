9102956239a820af781f301410d0065fc5b5a44f
denisprado <denisforigo@gmail.com>
2019-08-21 15:58:27 -0300

    authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from "react";
 import jsonServerProvider from "ra-data-json-server";
-import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
+import React from "react";
+import { Admin, fetchUtils, Resource } from "react-admin";
+import { AlbumCreate, AlbumEdit, AlbumList } from "./albums";
+import authProvider from './authProvider';
+import { FileCreate, FileEdit, FileList } from "./files";
+//import LoginWithTheme from './myLoginPage';
+import MyLogoutButton from './MyLogoutButton';
+import { PageCreate, PageEdit, PageList } from "./pages";
 import { UserList } from "./user";
-import { PageList, PageEdit, PageCreate } from "./pages";
-import { AlbumList, AlbumEdit, AlbumCreate } from "./albums";
-import { FileList, FileEdit, FileCreate } from "./files";
 
-const dataProvider = jsonServerProvider("http://localhost:3333");
+
+const httpClient = (url, options = {}) => {
+  if (!options.headers) {
+    options.headers = new Headers({ Accept: 'application/json' });
+  }
+  const token = localStorage.getItem('token');
+  options.headers.set('Authorization', `Bearer ${token}`);
+  return fetchUtils.fetchJson(url, options);
+}
+const dataProvider = jsonServerProvider("http://localhost:3333", httpClient);
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin
+    dataProvider={dataProvider}
+    //loginPage={LoginWithTheme}
+    logoutButton={MyLogoutButton}
+    authProvider={authProvider}>
     <Resource name="users" list={UserList} />
     <Resource
       name="files"
---
+++ src/authProvider.js
@@ -0,0 +1,42 @@
+import { AUTH_LOGIN, AUTH_ERROR, AUTH_LOGOUT, AUTH_CHECK } from 'react-admin';
+
+export default (type, params) => {
+  if (type === AUTH_LOGOUT) {
+    localStorage.removeItem('token');
+    return Promise.resolve();
+  }
+
+  if (type === AUTH_ERROR) {
+    const status = params.status;
+    if (status === 401 || status === 403) {
+      localStorage.removeItem('token');
+      return Promise.reject();
+    }
+    return Promise.resolve();
+  }
+
+  if (type === AUTH_CHECK) {
+    return localStorage.getItem('token') ? Promise.resolve() : Promise.reject();
+  }
+
+  if (type === AUTH_LOGIN) {
+    const { username, password } = params;
+    const request = new Request('http://localhost:3333/sessions', {
+      method: 'POST',
+      body: JSON.stringify({ name: username, password }),
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+    })
+    console.log(params)
+    return fetch(request)
+      .then(response => {
+        if (response.status < 200 || response.status >= 300) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })
+      .then(({ token }) => {
+        localStorage.setItem('token', token);
+      });
+  }
+  return Promise.resolve();
+}
