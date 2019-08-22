d7288ff589f0f8c266163ca3111cce7bd43e65f6
denisprado <denisforigo@gmail.com>
2019-08-22 16:56:11 -0300

    authentication

--- src/addUploadFeature.js
+++ src/addUploadFeature.js
@@ -1,55 +1,34 @@
-// in addUploadFeature.js
-/**
- * Convert a `File` object returned by the upload input into a base 64 string.
- * That's not the most optimized way to store images in production, but it's
- * enough to illustrate the idea of data provider decoration.
- */
-const convertFileToBase64 = file =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    //reader.readAsDataURL(file.rawFile);
 
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = reject;
-  });
+const convertFileToBase64 = (file, reader) => new Promise((resolve, reject) => {
+
+  reader.append('files', file.rawFile);
+
+  resolve(reader);
+
+});
 
 /**
- * For posts update only, convert uploaded image in base 64 and attach it to
- * the `picture` sent property, with `src` and `title` attributes.
- */
-const addUploadFeature = requestHandler => (type, resource, params) => {
-  if (type === "UPDATE" && resource === "posts") {
-    // notice that following condition can be true only when `<ImageInput source="files" />` component has parameter `multiple={true}`
-    // if parameter `multiple` is false, then data.files is not an array, but single object
+* For posts update only, convert uploaded image in base 64 and attach it to
+* the `picture` sent property, with `src` and `title` attributes.
+*/
+const addUploadCapabilities = requestHandler => (type, resource, params) => {
+  if (type === 'CREATE' && resource === 'files') {
     if (params.data.files && params.data.files.length) {
       // only freshly dropped files are instance of File
-      const formerPictures = params.data.files.filter(
-        p => !(p.rawFile instanceof File)
-      );
-      const newPictures = params.data.files.filter(
-        p => p.rawFile instanceof File
-      );
-
-      return Promise.all(newPictures.map(convertFileToBase64))
-        .then(base64Pictures =>
-          base64Pictures.map((picture64, index) => ({
-            src: picture64,
-            title: `${newPictures[index].title}`
-          }))
-        )
-        .then(transformedNewPictures =>
-          requestHandler(type, resource, {
-            ...params,
-            data: {
-              ...params.data,
-              files: [...transformedNewPictures, ...formerPictures]
-            }
-          })
-        );
+      const formerFiles = params.data.files.filter(p => !(p.rawFile instanceof File));
+      const newFiles = params.data.files.filter(p => p.rawFile instanceof File);
+      const reader = new FormData(params.data.files);
+      return requestHandler(type, resource, {
+        ...params,
+        data: {
+          ...params.data,
+          files: reader,
+        },
+      });
     }
   }
-  // for other request types and resources, fall back to the default request handler
+
   return requestHandler(type, resource, params);
 };
 
-export default addUploadFeature;
+export default addUploadCapabilities;
