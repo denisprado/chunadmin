cade4ee47fe282096a9b7bdde97277ac510c6d3a
Denis <denisforigo@gmail.com>
2019-10-22 20:31:11 -0300

    adding formData Capabilities

--- src/App.js
+++ src/App.js
@@ -2,33 +2,36 @@ import jsonServerProvider from "ra-data-json-server";
 import React from "react";
 import { Admin, fetchUtils, Resource } from "react-admin";
 import { AlbumCreate, AlbumEdit, AlbumList } from "./albums";
-import authProvider from './authProvider';
+import authProvider from "./authProvider";
 import { FileCreate, FileEdit, FileList } from "./files";
 //import LoginWithTheme from './myLoginPage';
-import MyLogoutButton from './MyLogoutButton';
+import MyLogoutButton from "./MyLogoutButton";
 import { PageCreate, PageEdit, PageList } from "./pages";
 import { UserList } from "./user";
-// import addUploadFeature from './addUploadFeature';
+import addUploadFeature from "./addUploadFeature";
 
 const httpClient = (url, options = {}) => {
   if (!options.headers) {
-    options.headers = new Headers({ Accept: 'application/json' });
+    options.headers = new Headers({ Accept: "application/json" });
   }
-  const token = localStorage.getItem('token');
-  options.headers.set('Authorization', `Bearer ${token}`);
+  const token = localStorage.getItem("token");
+  options.headers.set("Authorization", `Bearer ${token}`);
   return fetchUtils.fetchJson(url, options);
-}
-const dataProvider = jsonServerProvider(process.env.REACT_APP_API_URL, httpClient);
-//const uploadCapableDataProvider = addUploadFeature(dataProvider);
-
+};
+const dataProvider = jsonServerProvider(
+  process.env.REACT_APP_API_URL,
+  httpClient
+);
+const uploadCapableDataProvider = addUploadFeature(dataProvider);
 
 const App = () => (
   <Admin
-    dataProvider={dataProvider}
-    //dataProvider={uploadCapableDataProvider}
+    //dataProvider={dataProvider}
+    dataProvider={uploadCapableDataProvider}
     //loginPage={LoginWithTheme}
     logoutButton={MyLogoutButton}
-    authProvider={authProvider}>
+    authProvider={authProvider}
+  >
     <Resource name="users" list={UserList} />
     <Resource
       name="files"
--- src/addUploadFeature.js
+++ src/addUploadFeature.js
@@ -1,81 +1,33 @@
-const axios = require('axios')
+const addUploadCapabilities = requestHandler => (type, resource, params) => {
+  if (type === "CREATE" && resource === "files") {
+    // console.log(params.data.files);
+    console.log(params.data);
+    if (params.data.files) {
+      const files = params.data.files.map(file => file.rawFile);
+      const allFiles = files.filter(
+        file =>
+          file.type === "image/png" ||
+          file.type === "image/jpeg" ||
+          file.type === "image/jpg"
+      );
 
-const convertFileToBase64 = file => new Promise((resolve, reject) => {
-  const reader = new FileReader();
-  reader.readAsDataURL(file.rawFile);
+      const file = allFiles[0];
 
-  reader.onload = () => resolve(reader.result);
-  reader.onerror = reject;
-});
+      console.log(params.data);
+      // console.log(bookFile.preview);
 
-function convertToMultPart(filesCode64) {
+      var formData = new FormData();
+      const data = params.data;
+      // details
 
-  let fileMultPart = filesCode64.map(code64 => {
-    const boundary = '------multipartformboundary' + (new Date()).getTime(),
-      dashdash = '--',
-      crlf = '\r\n';
+      // files
+      formData.append("files", file);
 
-    return dashdash + boundary + crlf +
-      'Content-Disposition: form-data; name="files";' + crlf + crlf + code64.src + crlf + dashdash + boundary + dashdash + crlf;
-
-  });
-
-  alert(fileMultPart)
-
-  return (fileMultPart);
-}
-
-
-function sendData(data) {
-  const XHR = new XMLHttpRequest();
-  var boundary = '------multipartformboundary' + (new Date()).getTime()
-
-  XHR.open("POST", "http://localhost:3333/files");
-  // Define what happens on successful data submission
-  XHR.addEventListener("load", function (event) {
-    alert("Yeah! Data sent and response loaded.");
-  });
-
-  // Define what happens in case of error
-  XHR.addEventListener("error", function (event) {
-    alert("Oops! Something went wrong.");
-  });
-  XHR.setRequestHeader("Content-type", "multipart/form-data; boundary=" + boundary);
-  XHR.setRequestHeader("Content-length", data.length);
-  XHR.setRequestHeader("Connection", "close");
-  // Set up our request
-
-  //XHR.setRequestHeader("X-Api-Authorization", window.access_token);
-  // Send our FormData object; HTTP headers are set automatically
-  XHR.send(data);
-  alert("SendData: " + XHR.readyState)
-  return XHR.readyState;
-}
-
-const addUploadFeature = requestHandler => (type, resource, params) => {
-  if (type === 'CREATE' && resource === 'files') {
-    // notice that following condition can be true only when `<ImageInput source="files" />` component has parameter `multiple={true}`
-    // if parameter `multiple` is false, then data.files is not an array, but single object
-    if (params.data.files && params.data.files.length) {
-      // only freshly dropped files are instance of File
-      const newFiles = params.data.files.filter(p => p.rawFile instanceof File);
-
-      return Promise.all(newFiles.map(convertFileToBase64))
-        .then(base64Files => base64Files.map((picture64, index) => ({
-          src: picture64,
-          title: `${newFiles[index].title}`,
-        })))
-        .then(convertedFilesCode64 =>
-          convertToMultPart(convertedFilesCode64)
-        )
-        .then((multFormDataFiles) =>
-          sendData(multFormDataFiles)
-        )
-        .then(resFormData => requestHandler(type, resource, params));
+      params.data = formData;
     }
   }
-  // for other request types and resources, fall back to the default request handler
+
   return requestHandler(type, resource, params);
 };
 
-export default addUploadFeature;
+export default addUploadCapabilities;
